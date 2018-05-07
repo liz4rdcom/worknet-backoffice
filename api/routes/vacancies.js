7d60b32f672d2ec281c9aa830c0096e282dd976b
irakli jiishkariani <irakli2692@gmail.com>
2018-05-07 10:58:11 +0400

    vacancies advanced search api

--- api/routes/vacancies.js
+++ api/routes/vacancies.js
@@ -27,6 +27,16 @@ router.get('/own', async (req, res, next) => {
   }
 })
 
+router.get('/advancedSearch', async (req, res, next) => {
+  try {
+    let result = await vacancyInteractor.advancedSearch(req.body)
+
+    next({result})
+  } catch (error) {
+    next({error})
+  }
+})
+
 router.get('/:id', async (req, res, next) => {
   try {
     let result = await vacancyInteractor.getById(req.params.id)
