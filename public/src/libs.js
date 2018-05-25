63357dbdc3cb420a57a1746b663f32cc44ce8732
Shalva Aroshidze <shalva.aroshidze@gmail.com>
2018-05-25 14:03:37 +0400

    lib uses webapi not axios

--- public/src/libs.js
+++ public/src/libs.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import webApi from './webApi'
 
 async function fetchFormalEducationLevels () {
   if (this.formalEducationLevels) return this.formalEducationLevels
-  let response = await axios.get('api/libs/formalEducationLevels')
+  let response = await webApi.get('api/libs/formalEducationLevels')
   this.formalEducationLevels = response.data
   return this.formalEducationLevels
 }
